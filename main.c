a9748889b7137031035a1c5b299cfe903be9cdba
Tech Krowd <techkrowd@gmail.com>
2016-02-21 11:33:22 +0100

    Added files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+//0 1 1 2 3 5 8
+
+
+void fibonacci(int penultimo, int ultimo, int n){
+	int actual;
+
+	printf("%d ",ultimo);
+	if(n>1){
+		actual = penultimo + ultimo;
+		n--;
+		fibonacci(ultimo,actual,n);
+	}
+}
+
+int main (int argc, char *argv[])
+{
+
+	fibonacci (0, 1, 3);
+	return 0;
+}
+
