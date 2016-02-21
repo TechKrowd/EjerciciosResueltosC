efb34ddbdbfa72f72624e3f4396d47eb9aa486ea
Tech Krowd <techkrowd@gmail.com>
2016-02-21 11:36:39 +0100

    Added files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int factorial (int numero){
+	if(numero < 1){
+		return 0;
+	}else if(numero == 1){
+		return 1;
+	}else{
+		return numero * factorial(numero-1);
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	
+	//5! -> 1*2*3*4*5 -> 5 * 4!
+	//4! -> 1*2*3*4   -> 4 * 3!
+	//3! -> 1*2*3     -> 3 * 2!
+	//2! -> 1*2       -> 2 * 1!
+	//1! -> 1
+	int numero;
+	int resultado;
+	printf("Introduce un numero:");
+	scanf("%d",&numero);
+	resultado = factorial(numero);
+
+	printf("El factorial de %d es %d\n", numero, resultado);		
+	return 0;
+}
+
