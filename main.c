a6f4b3bcd86e3288326ae5726614301c76b3bd3e
Tech Krowd <techkrowd@gmail.com>
2016-02-21 11:35:50 +0100

    Added files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	//1*2*3*4*5
+
+	int numero=0;
+	int resultado=1;
+	int i=0;
+
+	printf("Introduce el numero para calcular el factorial: ");
+	scanf("%d", &numero);
+
+	for(i=2; i<=numero; i++){
+		resultado = resultado * i;
+	}
+
+	printf("El factorial de %d es %d\n", numero, resultado);
+
+	return 0;
+}
+
